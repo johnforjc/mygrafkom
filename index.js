ac1fd167efef3de1b9630a65484a4e84654ce8b7
johnforjc <44581271+johnforjc@users.noreply.github.com>
2019-10-31 00:27:54 +0700

    bisa mantul

--- index.js
+++ index.js
@@ -1,230 +1,183 @@
-(function() {
-
-    var canvas, gl, program, nKiri, nKanan;
+(function(){
+    var canvas, gl, program, nWord, pindahX =0.0053, pindahY = 0.0053;
 
     glUtils.SL.init({ callback:function() { main(); } });
-  
+
     function main() {
-      // Get canvas element and check if WebGL enabled
-      canvas = document.getElementById("glcanvas");
-      gl = glUtils.checkWebGL(canvas);
-  
-      initGlSize();
-  
-      // Initialize the shaders and program
-      var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex),
-          fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
-          program = glUtils.createProgram(gl, vertexShader, fragmentShader);
-      gl.useProgram(program);
+        // Get canvas element and check if WebGL enabled
+        canvas = document.getElementById("glcanvas");
+        gl = glUtils.checkWebGL(canvas);
 
-      var gambarKiri=1.00;
-      var gambarKiriUniformLocation = gl.getUniformLocation(program, 'gambarKiri');
-      gl.uniform1f(gambarKiriUniformLocation, gambarKiri);
-      
-      var thetaUniformLocation = gl.getUniformLocation(program, 'theta');
-      var theta = 0;
-      gl.uniform1f(thetaUniformLocation, theta);
-      
-      var scaleXUniformLocation = gl.getUniformLocation(program, 'scaleX');
-      var scaleX = 1.0;
-      gl.uniform1f(scaleXUniformLocation, scaleX);
-      
-      var scaleYUniformLocation = gl.getUniformLocation(program, 'scaleY');
-      var scaleY = 1.0;
-      gl.uniform1f(scaleYUniformLocation, scaleY);
+        initGlSize();
 
-      nKiri = bufferSebelahKiri(gl);
-      nKanan = bufferSebelahKanan(gl);
-  
-      resizer();
+        // Initialize the shaders and program
+        var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex),
+        fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
+        program = glUtils.createProgram(gl, vertexShader, fragmentShader);
+        gl.useProgram(program);
 
-      function initGlSize() {
+        nWord = initWordVertices();
 
-
-        var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
-        // Fullscreen if not set
-        if (width) {
-          gl.maxWidth = width;
-        }
-        if (height) {
-          gl.maxHeight = height;
-        }
-      }
-
-      function render(){
-        theta+=0.0053;
-        gl.uniform1f(thetaUniformLocation, theta);
-
-        gl.clearColor(0.364, 0.305, 1, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gambarKiri = 1.00;
-        gl.uniform1f(gambarKiriUniformLocation, gambarKiri);
-        gl.drawArrays(gl.LINE_LOOP, 0, nKiri);
-
-        if (scaleX >= 1.0) melebar = -1.0;
-        else if (scaleX <= -1.0) melebar = 1.0;
-        scaleX += 0.0053 * melebar;
+        var scaleXUniformLocation = gl.getUniformLocation(program, 'scaleX');
+        var scaleX = 1.0;
         gl.uniform1f(scaleXUniformLocation, scaleX);
 
-        gambarKiri = 0.00;
-        gl.uniform1f(gambarKiriUniformLocation, gambarKiri);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, nKanan);
+        var constantUniformLocation = gl.getUniformLocation(program, 'constant');
+        var constant = 0.5;
+        gl.uniform1f(constantUniformLocation, constant);
 
-        requestAnimationFrame(render);
-      }
-    
-      function draw() {
-        gl.clearColor(0.364, 0.305, 1, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gambarKiri = 1.00;
-        gl.uniform1f(gambarKiriUniformLocation, gambarKiri);
-        gl.drawArrays(gl.LINE_LOOP, 0, nKiri);
-        gambarKiri = 0.00;
-        gl.uniform1f(gambarKiriUniformLocation, gambarKiri);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, nKanan);
-  
+        var jalanXUniformLocation = gl.getUniformLocation(program, 'jalanX');
+        var jalanX = 0.00;
+        gl.uniform1f(jalanXUniformLocation, jalanX);
+
+        var jalanYUniformLocation = gl.getUniformLocation(program, 'jalanY');
+        var jalanY = 0.00;
+        gl.uniform1f(jalanYUniformLocation, jalanY);
+
         render();
-      }
-  
-      function bufferSebelahKiri() {
-        var vertices=[];
-        var garis=[
-          // -0.40 , -0.20,
-          // -0.40 , +0.40,
-          // -0.25 , +0.40,
-          // -0.25 , +0.50,
-          // -0.65 , +0.50,
-          // -0.65 , +0.40,
-          // -0.50 , +0.40,
-          // -0.50 , -0.20,
-          0.05    ,   -0.35,
-          0.05    ,   0.25,
-          0.2	    ,   0.25,
-          0.2	    ,   0.35,
-          -0.2    ,   0.35,
-          -0.2    ,   0.25,
-          -0.05   ,   0.25,
-          -0.05   ,   -0.35,
-        ];
-        
-        var vertexBuffer = gl.createBuffer(),
-          lingkaran1 = [], lingkaran2=[];
-  
-        for (var i=90.0; i<=270; i+=1) {
-          var j = i * Math.PI / 180;
-          var vert1 = [
-            Math.sin(j)*0.1-0.15,
-            Math.cos(j)*0.1-0.35,
-          ];
-          lingkaran1=lingkaran1.concat(vert1);
-  
-          var vert2 = [
-            Math.sin(j)*0.2-0.15,
-            Math.cos(j)*0.2-0.35,
-          ];
-          vert2.reverse();
-          lingkaran2=lingkaran2.concat(vert2);
-        }
-        lingkaran2.reverse();
-        lingkaran1=lingkaran1.concat(lingkaran2);
-        vertices=vertices.concat(garis);
-        vertices=vertices.concat(lingkaran1);
-        
-        var n = vertices.length / 2;
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    
-        var vPositionKiri = gl.getAttribLocation(program, 'vPositionKiri');
-        gl.enableVertexAttribArray(vPositionKiri);
-        gl.vertexAttribPointer(vPositionKiri, 2, gl.FLOAT, false, 0, 0);
-    
-        return n;
-      }
-  
-      function bufferSebelahKanan() {
-        var vertices=[];
-        var bagian_atas=[
-          // +0.40 , +0.50,
-          // +0.40 , +0.40,
-          // +0.70 , +0.40,
-          // +0.70 , +0.50,
-          // +0.40 , +0.50,
+        window.addEventListener('resize', resizer);
 
-          -0.15 , +0.30,
-          -0.15 , +0.20,
-          +0.15 , +0.20,
-          +0.15 , +0.30,
-          -0.15 , +0.30,
-        ];
-  
-        var bagian_tegak=[
-          // +0.50 , +0.50,
-          // +0.60 , +0.50,
-          // +0.60 , -0.20,
-          // +0.50 , -0.20,
-          // +0.50 , +0.50,
+        var vertexKiriTerjauh  = -0.15;
+        var vertexKananTerjauh = 0.15;
+        var vertexAtasTerjauh  = 0.30;
+        var vertexBawahTerjauh = -0.6;
+        var ukuranTerjauh = 0.5;
 
-          -0.05 , +0.30,
-          +0.05 , +0.30,
-          +0.05 , -0.40,
-          -0.05 , -0.40,
-          -0.05 , +0.30,
-        ];
-        
-        var vertexBuffer = gl.createBuffer(),
-          lingkaran =[];
-  
-        for (var i=90.0; i<=270; i+=1) {
-          var j = i * Math.PI / 180;
-          var vert1 = [
-            Math.sin(j)*0.2-0.15,
-            Math.cos(j)*0.2-0.40,
-          ];
-          lingkaran=lingkaran.concat(vert1);
-  
-          var vert2 = [
-            Math.sin(j)*0.1-0.15,
-            Math.cos(j)*0.1-0.40,
-          ];
-          lingkaran=lingkaran.concat(vert2);
+        function bounceChecking(){
+            // Checking X koordinat
+            if(constant*scaleX*vertexKiriTerjauh+jalanX>=ukuranTerjauh)
+            {
+                jalanX = ukuranTerjauh - constant*scaleX*vertexKiriTerjauh;
+                pindahX = -1*pindahX;
+            }
+            else if(constant*scaleX*vertexKananTerjauh+jalanX>=ukuranTerjauh)
+            {
+                jalanX = ukuranTerjauh - constant*scaleX*vertexKananTerjauh;
+                pindahX = -1*pindahX;
+            }
+            else if(constant*scaleX*vertexKiriTerjauh+jalanX<= -1*ukuranTerjauh)
+            {
+                jalanX = (-1 * ukuranTerjauh - constant*scaleX*vertexKiriTerjauh);
+                pindahX = -1*pindahX;
+            }
+            else if(constant*scaleX*vertexKananTerjauh+jalanX<= -1*ukuranTerjauh)
+            {
+                jalanX = (-1 * ukuranTerjauh - constant*scaleX*vertexKananTerjauh);
+                pindahX = -1*pindahX;   
+            }
+
+            // Checking Y koordinat
+            if(constant * vertexAtasTerjauh +jalanY>=ukuranTerjauh)
+            {
+                jalanY = ukuranTerjauh - constant*vertexAtasTerjauh;
+                pindahY = -1*pindahY;
+            }
+            else if(constant * vertexBawahTerjauh+jalanY<= -1*ukuranTerjauh)
+            {
+                jalanY = (-1 * ukuranTerjauh - constant*vertexBawahTerjauh);
+                pindahY = -1*pindahY;
+            }
         }
-        bagian_atas=bagian_atas.concat(bagian_tegak);
-        vertices=vertices.concat(bagian_atas);
-        vertices=vertices.concat(lingkaran);
-        
-        var n = vertices.length / 2;
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+        function render(){
+            gl.clearColor(0.364, 0.305, 1, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            if (scaleX >= 1.0) melebar = -1.0;
+            else if (scaleX <= -1.0) melebar = 1.0;
+            scaleX += 0.0053 * melebar;
+
+            jalanX += pindahX;
+            jalanY += pindahY;
+
+            bounceChecking();
+
+            gl.uniform1f(jalanYUniformLocation, jalanY);
+            gl.uniform1f(jalanXUniformLocation, jalanX);
+
+            gl.uniform1f(scaleXUniformLocation, scaleX);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, nWord);
     
-        var vPositionKanan = gl.getAttribLocation(program, 'vPositionKanan');
-        gl.enableVertexAttribArray(vPositionKanan);
-        gl.vertexAttribPointer(vPositionKanan, 2, gl.FLOAT, false, 0, 0);
+            requestAnimationFrame(render);
+          }
     
-        return n;
-      }
-    
-      function resizer() {
-    
-        var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
-        if (!width || width < 0) {
-          canvas.width = window.innerWidth;
-          gl.maxWidth = window.innerWidth;
+        function initGlSize() {
+            var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
+            // Fullscreen if not set
+            if (width) {
+                gl.maxWidth = width;
+            }
+            if (height) {
+                gl.maxHeight = height;
+            }
         }
-        if (!height || height < 0) {
-          canvas.height = window.innerHeight;
-          gl.maxHeight = window.innerHeight;
+    
+        function initWordVertices() {
+            var vertices=[];
+            var bagian_atas=[
+                -0.15 , +0.30,
+                -0.15 , +0.20,
+                +0.15 , +0.20,
+                +0.15 , +0.30,
+                -0.15 , +0.30,
+            ];
+            
+            var bagian_tegak=[
+                -0.05 , +0.30,
+                +0.05 , +0.30,
+                +0.05 , -0.40,
+                -0.05 , -0.40,
+                -0.05 , +0.30,
+            ];
+            
+            var vertexBuffer = gl.createBuffer(),
+            lingkaran =[];
+            
+            for (var i=90.0; i<=270; i+=1) {
+                var j = i * Math.PI / 180;
+                var vert1 = [
+                    Math.sin(j)*0.2-0.15,
+                    Math.cos(j)*0.2-0.40,
+                ];
+                lingkaran=lingkaran.concat(vert1);
+                
+                var vert2 = [
+                    Math.sin(j)*0.1-0.15,
+                    Math.cos(j)*0.1-0.40,
+                ];
+                lingkaran=lingkaran.concat(vert2);
+            }
+            bagian_atas=bagian_atas.concat(bagian_tegak);
+            vertices=vertices.concat(bagian_atas);
+            vertices=vertices.concat(lingkaran);
+            
+            var n = vertices.length / 2;
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            
+            var vPosition = gl.getAttribLocation(program, 'vPosition');
+            gl.enableVertexAttribArray(vPosition);
+            gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+            
+            return n;
         }
     
-        var min = Math.min.apply( Math, [gl.maxWidth, gl.maxHeight, window.innerWidth, window.innerHeight]);
-        canvas.width = min;
-        canvas.height = min;
-    
-        gl.viewport(0, 0, canvas.width, canvas.height);
-    
-        draw();
-      }
-    
-      window.addEventListener('resize', resizer);
-    }
-  
-  })();
+        function resizer() {
+            var width = canvas.getAttribute("width"), height = canvas.getAttribute("height");
+            if (!width || width < 0) {
+                canvas.width = window.innerWidth;
+                gl.maxWidth = window.innerWidth;
+            }
+            if (!height || height < 0) {
+                canvas.height = window.innerHeight;
+                gl.maxHeight = window.innerHeight;
+            }
+            
+            var min = Math.min.apply( Math, [gl.maxWidth, gl.maxHeight, window.innerWidth, window.innerHeight]);
+            canvas.width = min;
+            canvas.height = min;
+            
+            gl.viewport(0, 0, canvas.width, canvas.height);
+        }
+    }   
+}) ();
--- vertex.glsl
+++ vertex.glsl
@@ -1,40 +1,24 @@
 precision mediump float;
 
-attribute vec2 vPositionKiri;
-attribute vec2 vPositionKanan;
-uniform float gambarKiri;
-uniform float theta;
+attribute vec2 vPosition;
 uniform float scaleX;
-uniform float scaleY;
+uniform float constant;
+uniform float jalanX;
+uniform float jalanY;
 
 void main() {
-  mat4 translasiKiri = mat4
-    (1.0, 0.0, 0.0, 0.0,
-     0.0, 1.0, 0.0, 0.0,
+  mat4 translasi = mat4
+    (constant, 0.0, 0.0, 0.0,
+     0.0, constant, 0.0, 0.0,
      0.0, 0.0, 1.0, 0.0,
-     -0.4, 0.0, 0.0, 1.0 
+     jalanX, jalanY, 0.0, 1.0 
     );
-
-  mat4 translasiKanan = mat4
-    (1.0, 0.0, 0.0, 0.0,
-     0.0, 1.0, 0.0, 0.0,
-     0.0, 0.0, 1.0, 0.0,
-     0.4, 0.05, 0.0, 1.0 
-    );
-
-  mat4 rotationMatrix = mat4(
-    cos(theta), sin(theta), 0.0, 0.0,
-    -sin(theta), cos(theta), 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0
-  );
 
   mat4 scalationMatrix = mat4(
     scaleX, 0.0, 0.0, 0.0,
-    0.0, scaleY, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
-  if(gambarKiri==1.00) gl_Position = translasiKiri  * rotationMatrix * vec4(vPositionKiri, 0.0, 1.0);
-  if(gambarKiri==0.00) gl_Position = translasiKanan * scalationMatrix * vec4(vPositionKanan, 0.0, 1.0);
+  gl_Position = translasi  * scalationMatrix * vec4(vPosition, 0.0, 1.0);
 }
